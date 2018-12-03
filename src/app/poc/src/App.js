e718c1f82eaada2bd576b68dde29a310fb587726
luis.takahashi <luis.takahashi@ifood.com.br>
2018-12-03 11:17:40 -0200

    Added app by create-react-app

---
+++ src/app/poc/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+const fetchRequest = () => {
+  const url = 'http://localhost:8080/';
+  return fetch(url, {
+    headers: {
+      abelha: 'azul',
+      batata: 'verde',
+      aaaaa: 'opa meu jovem',
+      opaaar: 'amodeuzu',
+    }
+  })
+  .then(function(response) {
+    return response.blob();
+  })
+  .then(function(data) {
+    console.log(data);
+    return data;
+  })
+  .catch((aa) => {
+    console.error(aa);
+  })
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  componentDidMount() {
+    fetchRequest().then(resp => 
+      {
+        this.setState({
+          iframeSrc: URL.createObjectURL(resp)
+        });
+
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h4>
+            POC - Iframe etc
+          </h4>
+          <div className="App-iframe">
+            {
+              this.state.iframeSrc && (
+                <iframe src={this.state.iframeSrc} name="POC-iframe"></iframe>
+              )
+            }
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
